603be3882dd49a7c547404b67489ef6937c475bd
Jon Atlas <jjjatlas@hotmail.com>
2023-09-26 14:25:27 +0100

    Update starter items

--- atlas-ecommerce-store/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import {
-  About
-} from './pages';
-
-
-const App = () => {
-  return <h1 className='text-3xl font-bold underline'>Tailwind project</h1>;
-};
-export default App;
--- atlas-ecommerce-store/src/pages/About.jsx
+++
@@ -1,4 +0,0 @@
-const About = () => {
-    return <h1 className='text-4xl'>About</h1>;
-  };
-  export default About;
